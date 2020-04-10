85427143a16acd226aa20d319c90169d61749c26
Tom Ellis <thomas.ellis@gmi.oeaw.ac.at>
2020-04-10 11:00:55 +0200

    Changed plotQTL_ladder.R to only add the ladder, but not dashed lines between tracks. Updated to 0.6.2

--- R/plotQTL_ladder.R
+++ R/plotQTL_ladder.R
@@ -1,7 +1,6 @@
 #' Plot marker positions in a plotQTL plot.
 #' 
-#' Plots a solid axis with marker locations to the left of each chromosome track,
-#' and dashed grey dividers between lanes.
+#' Plots a solid axis with marker locations to the left of each chromosome track.
 #' 
 #' @param plotQTL A plotQTL object from plotQTL().
 #' @param marker_tick_width Width of marker labels.
@@ -20,14 +19,4 @@ function(plotQTL, marker_tick_width=0.5, col='gray50', lty='dashed', ...){
     segments(chr_xv[i],                   -plotQTL$map[[i]],
              chr_xv[i]-marker_tick_width, -plotQTL$map[[i]], lwd=0.5)
   }
-  
-  
-  # plot thin horizontal dividers between plotted chromosomes
-  xv <- as.vector(plotQTL$lane_margins[-1,])
-  yv <- -rep(plotQTL$track_lengths, each=plotQTL$nlanes)
-  segments(x0 = xv,
-           y1 = 0,
-           x1 = xv,
-           y0 = yv,
-           lty = lty, col = col, ...)
 }
