004cec984a70c8d572672364740d5d3d3654eae4
Tom Ellis <thomas.ellis@gmi.oeaw.ac.at>
2021-03-10 14:33:52 +0100

    Added support for modifying bg colour in plotQTL_qtl with the argument `bg`.  Changes to be committed: 	modified:   DESCRIPTION 	modified:   R/plotQTL_qtl.R

--- R/plotQTL_qtl.R
+++ R/plotQTL_qtl.R
@@ -20,12 +20,15 @@
 #' @param lane Integer indicating which lane to plot to.
 #' @param fill_cutoff Threshold length of a confidence interval on QTL positions, above which
 #' points will be empty, and below which points will be filled.
-#' @param col Plotting colour. Defaults to black.
+#' @param col Plotting colour for point estimates and credible intervals. Defaults to black.
+#' @param bg Plotting colour for the background of point estimates. This can be a single
+#' entry or a vector with an entry for each QTL to be plotted. Defaults to `col`. If
+#' `fill_cutoff` is also given, this overrides `bg`.
 #' @param ... Additional plotting commands passed to points() and segments().
 #' 
 #' @export
 plotQTL_qtl <-
-  function(plotQTL, qtl_object=NULL, model_fit=NULL, cluster_qtl=NULL, lane, fill_cutoff=NULL, col=1, ...){
+  function(plotQTL, qtl_object=NULL, model_fit=NULL, cluster_qtl=NULL, lane, fill_cutoff=NULL, col=1, bg=col, ...){
     if(class(qtl_object) == "qtl" & class(model_fit) == "fitqtl" & is.null(cluster_qtl)){
       qtl_positions <- bayesint_table(qtl_object, model_fit)
       xv <- plotQTL$lane_centres[lane,match(qtl_positions$chr, colnames(plotQTL$lane_centres))]
@@ -38,19 +41,21 @@ plotQTL_qtl <-
       qtl_positions$pch[qtl_positions$effect_size ==0] <- 18
       
       # If fill_cutoff is NULL, set the maximum length to something longer than the longest interval.
-      if(is.null(fill_cutoff)) fill_cutoff <- max(qtl_positions$interval_length)+1
-      # create a vector for whether arrows should be filled white or the same colour as the outline
-      qtl_positions$bg <- numeric(nrow(qtl_positions))
-      qtl_positions$bg[qtl_positions$interval_length <  fill_cutoff] <- col
-      qtl_positions$bg[qtl_positions$interval_length >= fill_cutoff] <- 'white'
-      # add confidence intervals
+      if(is.numeric(fill_cutoff)){
+        # create a vector for whether arrows should be filled white or the same colour as the outline
+        bg <- numeric(nrow(qtl_positions))
+        bg[qtl_positions$interval_length <  fill_cutoff] <- col
+        bg[qtl_positions$interval_length >= fill_cutoff] <- 'white'
+      }
+      
+      # plot credible intervals
       segments(xv, -qtl_positions$max_bayesint, xv, -qtl_positions$min_bayesint, col=col, ...)
       # plot ML positions
-      points(xv, -qtl_positions$ML_bayesint, pch=qtl_positions$pch, col=col, bg=qtl_positions$bg, ...)
+      points(xv, -qtl_positions$ML_bayesint, pch=qtl_positions$pch, col=col, bg=bg, ...)
     }
     
     # Plot point estimates and credible intervals for a cluster object.
-    else if (is.null(qtl_object) & is.null(model_fit) &!is.null(cluster_qtl)){
+    else if (is.null(qtl_object) & is.null(model_fit) & ! is.null(cluster_qtl)){
       cluster_qtl <- cluster_qtl$summary
       xv <- plotQTL$lane_centres[lane, match(cluster_qtl$chr, colnames(plotQTL$lane_centres))] # x-axis values.
       
